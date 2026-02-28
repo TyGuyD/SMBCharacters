45f48463943816974554683103aa927ce9abdd75
TylerD <tyguy.demshar@gmail.com>
2026-02-28 02:51:49 -0600

    [1.05] Added exception handling

--- Program.cs
+++ Program.cs
@@ -1,84 +1,95 @@
 ﻿
 string file = "mario.csv";
 
-List<UInt64> Ids = [];
-List<string> Names = [];
-List<string?> Descriptions = [];
-List<string> Species = [];
-List<string> FirstAppearance = [];
-List<string> Year = [];
-
-StreamReader sr = new(file);
-
-sr.ReadLine();
-while (!sr.EndOfStream)
+if (!File.Exists(file))
 {
-    string? line = sr.ReadLine();
-    if (line is not null)
-    {
-        string[] characterDetails = line.Split(',');
-        Ids.Add(UInt64.Parse(characterDetails[0]));
-        Names.Add(characterDetails[1]);
-        Descriptions.Add(characterDetails[2]);
-        Species.Add(characterDetails[3]);
-        FirstAppearance.Add(characterDetails[4]);
-        Year.Add(characterDetails[5]);
-    }
+    // Will log error on later commit
 }
-sr.Close();
-
-string? choice;
-do
+else
 {
-    Console.WriteLine("1) Add Character");
-    Console.WriteLine("2) Display All Characters");
-    Console.WriteLine("Enter to quit");
+    List<UInt64> Ids = [];
+    List<string> Names = [];
+    List<string?> Descriptions = [];
+    List<string> Species = [];
+    List<string> FirstAppearance = [];
+    List<string> Year = [];
 
-    choice = Console.ReadLine();
-
-    if (choice == "1")
-    {
-        Console.Write("Name: ");
-        string Name = Console.ReadLine()!;
-
-        List<string> LowerCaseNames = Names.ConvertAll(n => n.ToLower());
-        if (!LowerCaseNames.Contains(Name.ToLower()))
+    try {
+        StreamReader sr = new(file);
+        sr.ReadLine();
+        while (!sr.EndOfStream)
         {
-            UInt64 id = Ids.Max() + 1;
-            Console.Write("Description: ");
-            string? description = Console.ReadLine();
-            Console.Write("Species: ");
-            string species = Console.ReadLine()!;
-            Console.Write("First Appearance: ");
-            string firstAppearance = Console.ReadLine()!;
-            Console.Write("Year: ");
-            string year = Console.ReadLine()!;
-            Ids.Add(id);
-            Names.Add(Name);
-            Descriptions.Add(description);
-            Species.Add(species);
-            FirstAppearance.Add(firstAppearance);
-            Year.Add(year);
-            using StreamWriter sw = new(file, append: true);
-            sw.WriteLine($"{id},{Name},{description},{species},{firstAppearance},{year}");
-            sw.Close();
+            string? line = sr.ReadLine();
+            if (line is not null)
+            {
+                string[] characterDetails = line.Split(',');
+                Ids.Add(UInt64.Parse(characterDetails[0]));
+                Names.Add(characterDetails[1]);
+                Descriptions.Add(characterDetails[2]);
+                Species.Add(characterDetails[3]);
+                FirstAppearance.Add(characterDetails[4]);
+                Year.Add(characterDetails[5]);
+            }
         }
-
+        sr.Close();
+    } 
+    catch {
+        // Will log error on later commit
     }
-    else if (choice == "2")
+
+    string? choice;
+    do
     {
-        Console.WriteLine();
-        for (int i = 0; i < Ids.Count; i++)
+        Console.WriteLine("1) Add Character");
+        Console.WriteLine("2) Display All Characters");
+        Console.WriteLine("Enter to quit");
+
+        choice = Console.ReadLine();
+
+        if (choice == "1")
         {
-            Console.WriteLine($"Id: {Ids[i]}");
-            Console.WriteLine($"Name: {Names[i]}");
-            Console.WriteLine($"Description: {Descriptions[i]}");
-            Console.WriteLine($"Species: {Species[i]}");
-            Console.WriteLine($"First Appearance: {FirstAppearance[i]}");
-            Console.WriteLine($"Year: {Year[i]}");
+            Console.Write("Name: ");
+            string Name = Console.ReadLine()!;
+
+            List<string> LowerCaseNames = Names.ConvertAll(n => n.ToLower());
+            if (!LowerCaseNames.Contains(Name.ToLower()))
+            {
+                UInt64 id = Ids.Max() + 1;
+                Console.Write("Description: ");
+                string? description = Console.ReadLine();
+                Console.Write("Species: ");
+                string species = Console.ReadLine()!;
+                Console.Write("First Appearance: ");
+                string firstAppearance = Console.ReadLine()!;
+                Console.Write("Year: ");
+                string year = Console.ReadLine()!;
+                Ids.Add(id);
+                Names.Add(Name);
+                Descriptions.Add(description);
+                Species.Add(species);
+                FirstAppearance.Add(firstAppearance);
+                Year.Add(year);
+                using StreamWriter sw = new(file, append: true);
+                sw.WriteLine($"{id},{Name},{description},{species},{firstAppearance},{year}");
+                sw.Close();
+            }
+
+        }
+        else if (choice == "2")
+        {
             Console.WriteLine();
+            for (int i = 0; i < Ids.Count; i++)
+            {
+                Console.WriteLine($"Id: {Ids[i]}");
+                Console.WriteLine($"Name: {Names[i]}");
+                Console.WriteLine($"Description: {Descriptions[i]}");
+                Console.WriteLine($"Species: {Species[i]}");
+                Console.WriteLine($"First Appearance: {FirstAppearance[i]}");
+                Console.WriteLine($"Year: {Year[i]}");
+                Console.WriteLine();
+            }
         }
-    }
-    
-    
-} while (choice == "1" || choice == "2");
+        
+        
+    } while (choice == "1" || choice == "2");
+}
--- obj/Debug/net8.0/SMBCharacters.AssemblyInfo.cs
+++ obj/Debug/net8.0/SMBCharacters.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SMBCharacters")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df8e2897d35e4e3db8487d6304c20013e1ce2abf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f83c247006eed98bf7e06f89408b071dd6e0b619")]
 [assembly: System.Reflection.AssemblyProductAttribute("SMBCharacters")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SMBCharacters")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
